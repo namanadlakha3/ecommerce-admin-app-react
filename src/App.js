74992fe15d93b71cc284a4a899e9931c2d62fd0e
Naman Adlakha <naman.adlakha3@gmail.com>
2024-11-22 16:48:57 +0530

    Implemented tailwind css

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<><h1 className="border-lime-200 border-2 bg-black text-red-50">Hello</h1></>);
